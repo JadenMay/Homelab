b8c9380ac7e17c1adeb92a771371f7a94dd862af
JadenMay <69321579+JadenMay@users.noreply.github.com>
2022-04-15 17:03:05 -0500

    Added Array of provisioned apps

--- SOHO_Windows10_Setup.ps1
+++ SOHO_Windows10_Setup.ps1
@@ -1,1 +1,10 @@
-#This script is used to get rid of Windows 10 bloatware and perform other duties to make home PCs optimized
+# This script is used to get rid of Windows 10 bloatware and perform other duties to make home PCs optimized
+https://www.repairwin.com/how-to-completely-remove-built-in-apps-from-windows-10/
+# Get all installed provisioned apps in an array
+
+
+$ProvisionedApps = Get-AppxProvisionedPackage -online
+$ProvisionedApps
+
+# Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'microsoft.microsoft3dviewer'} | Remove-AppxProvisionedPackage -online
+
