2e4b4b1202698b839e2bf053074f11ec32efaf2f
JadenMay <69321579+JadenMay@users.noreply.github.com>
2022-06-23 17:08:03 -0500

    Update DomainStuff.ps1

--- DomainStuff.ps1
+++ DomainStuff.ps1
@@ -1,13 +1,4 @@
 #Enable RDP
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-
-#New Domain Admin
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString 
-New-ADUser -Name test05 -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
-Add-ADGroupMember -Identity "Domain Admins" -Members test05
-
-#Install DC + ADDS
-Add-WindowsFeature AD-Domain-Services
-Install-ADDSForest -DomainName name.local -InstallDNS
+shutdown /r /f /t 0
