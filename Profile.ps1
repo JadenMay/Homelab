dd4b85144ea8074ef79e6a7ac65d00ad17b88a66
JadenMay <jadenj.may@gmail.com>
2022-07-14 17:13:36 -0500

    Change

---
+++ Profile.ps1
@@ -0,0 +1,15 @@
+Function Profile {
+    Clear-Host
+    Write-Host "#=========================================================================================#"
+    Write-Host "|                                                                                         |"
+    Write-Host "|        " -NoNewLine; Write-Host -foregroundcolor Green "Homelab Powershell Profile EST. 7/14/22" -NoNewLine; write-host "                                          |"
+    Write-Host "|                                                                                         |"
+    Write-Host "|        " -nonewline; write-host -foregroundcolor Cyan "balls" -NoNewline; write-host "                                                                            |"
+    Write-Host "|                                                                                         |"
+    Write-Host "#=========================================================================================#"
+}
+Profile
+
+Function Test {
+    
+}
