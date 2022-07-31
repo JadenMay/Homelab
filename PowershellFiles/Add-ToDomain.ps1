280cc481f573399adcabf2a614b94a680180ad50
JadenMay <jadenj.may@gmail.com>
2022-07-31 16:43:11 -0500

    Update Add-ToDomain.ps1

--- PowershellFiles/Add-ToDomain.ps1
+++ PowershellFiles/Add-ToDomain.ps1
@@ -1,23 +1,34 @@
 #Enable RDP
-Write-Host -ForegroundColor Green "Enabling RDP"
+Write-Host -ForegroundColor Green "Enabling RDP..."
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 #Enable PsRemoting
+Write-Host -ForegroundColor Green "Enabling PsRemoting..."
 Enable-PSRemoting -Force
 
 #Rename
-Write-Host -ForegroundColor Cyan "Name me pls: " -NoNewline
+Write-Host -ForegroundColor Cyan "Name this PC:" -NoNewline
 $Name = Read-Host
 Rename-Computer $Name
 
 #Set IP Address
-Write-Host -ForegroundColor Cyan "Gimme the IP: " -NoNewline
+Write-Host -ForegroundColor Green "Gathering network adapters..."
+$NetAdapterArr = @()
+$NetAdapterArr = get-netadapter -name * | select name,ifindex
+for($i=0;$i -lt $NetAdapterArr.count;$i++)
+{
+    Write-Host "$($i): $($NetAdapterArr[$i].name)"
+}
+Write-Host -ForegroundColor Cyan "Please select an adapter to configure:" -NoNewline
+$Selection = Read-Host
+Write-Host "You selected: $($Selection) which is $($NetAdapterArr[$Selection].name)"
+Write-Host -ForegroundColor Cyan "Gimme the IP 192.168.1.xxx" -NoNewline
 $EyePee = Read-Host
-New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $EyePee -PrefixLength 24 -DefaultGateway 192.168.1.1
+New-NetIPAddress -InterfaceAlias $NetAdapterArr[$Selection].name -IPAddress $EyePee -PrefixLength 24 -DefaultGateway 192.168.1.1
 
 #Set DNS
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 192.168.1.250
+Set-DnsClientServerAddress -InterfaceAlias $NetAdapterArr[$Selection].name -ServerAddresses 192.168.1.250
 
 #Join Domain
 Write-Host -ForegroundColor Magenta "Joining jdev..."
@@ -28,5 +39,4 @@ Write-Host -ForegroundColor Red "Restarting $env:Computername in 120 seconds. Hu
 shutdown /r /f /t 120
 
 #Activation
-\\hyperv01\HyperVFiles\MicrosoftActivationScripts_1.5\All-In-One-Version\MAS_1.5_AIO_CRC32_21D20776.cmd -Credential jdev\jdevadmin
-
+\\dc01\c$\Tools\MicrosoftActivationScripts_1.5\All-In-One-Version\MAS_1.5_AIO_CRC32_21D20776.cmd -Credential jdev\jdevadmin
