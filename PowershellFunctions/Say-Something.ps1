a164bab5fd5c94f9f1cd00195cfd9fee8e6fde98
JadenMay <jadenj.may@gmail.com>
2022-09-21 17:19:40 -0500

    yerrr

--- PowershellFunctions/Say-Something.ps1
+++ PowershellFunctions/Say-Something.ps1
@@ -1,7 +1,5 @@
 Add-Type -AssemblyName System.speech
-
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 Write-Host -ForegroundColor Red "What do you want to say: " -NoNewline
 $say = Read-Host
-
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak("$say")
+$speak.Speak("latency $say")
