fe988a89feb659180c754dd088b6ed39ce5f88b3
JadenMay <jadenj.may@gmail.com>
2022-09-28 16:11:02 -0500

    update

--- PowershellFunctions/Say-Something/Say-Something.ps1
+++ PowershellFunctions/Say-Something/Say-Something.ps1
@@ -1,13 +1,38 @@
+#Imports
 Add-Type -AssemblyName System.speech
 $poweroftalk = New-Object System.Speech.Synthesis.SpeechSynthesizer
-Write-Host -ForegroundColor Red "What do you want to say, press 1 for a song: " -NoNewline
-$say = Read-Host
-if($say -eq "1"){
+
+#Song List
+Write-Host -ForegroundColor Green "
+Song List:
+1 - Puffin On Zooties
+2 - Knife Talk"
+
+#Get the pick
+Write-Host -ForegroundColor Red "What do you want to say: " -NoNewline
+$input = Read-Host
+
+#Logic
+if($input -eq "1"){
+    #Puffin On Zooties
     $poweroftalk.rate = 4
     $rap = Get-Content D:\git\homelab\powershellfunctions\say-something\puffinonzooties.txt
-    $poweroftalk.Speak("latency $rap")
+}elseif($input -eq "2"){
+    #Knife Talk
+    $poweroftalk.rate = 2
+    $rap = Get-Content D:\git\homelab\powershellfunctions\say-something\knifetalk.txt
 }else{
-    Write-host -ForegroundColor Green "Speed (-10 to 10): " -NoNewline
-    $poweroftalk.rate = Read-Host
-    $poweroftalk.Speak("latency $say")
+    #User Inputted
+    Write-host -ForegroundColor Green "Speed -10 to 10 (default is 0): " -NoNewline
+    $yerrr = Read-Host
+    if($yerrr -eq ""){
+        $poweroftalk.rate = 0
+    }
+    else{
+        $poweroftalk.rate = $yerrr
+    }
+    $rap = $input
 }
+
+#Say it
+$poweroftalk.Speak($rap)
